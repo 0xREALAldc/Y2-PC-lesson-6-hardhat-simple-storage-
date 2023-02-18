866b3b423ee879dc5e1f912082703e0a317e1daa
0xREALaldc <andredonat@hotmail.com>
2023-02-18 18:57:06 -0300

    Add: interacting with contracts using hardhat, how to write a custom Task for hardhat, how to spin up a local Node using hardhat, using hardhat Console to interact with contracts, writting and running tests using mocha, using hardhat gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -13,9 +15,17 @@ module.exports = {
       accounts: [process.env.GOERLI_PRIVATE_KEY],
       chainId: 5,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      // accounts: , // we don't need to explicitly define the accounts, because when we run hardhart it'l give us the accounts
+      chainId: 31337, // even being considered different from the `default hardhat network` the chainId is the same
+    }
   },
   solidity: "0.8.7",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@
 // different from what we did using ethers in the other project, as hardhat wraps `ethers` inside their own package
 // we're going to import `ethers` from `hardhat` 
 // if you look inside the `package.json` you'll see the package `@nomiclabs/hardhat-ethers`
-const { ethers, run } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
@@ -13,6 +13,27 @@ async function main() {
   //to make sure we wait till it's deployed we can use the line below
   await simpleStorage.deployed()
   console.log(`Deployed contract to: ${simpleStorage.address}`)
+
+  // only verify the contract if we're running in GOERLI testnet and have a API key
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    // before running the verify function, we're going to set to wait the blockchain to mine a few blocks to make sure that 
+    // the contract is already mined on the blockchain
+    // we'll wait 6 blocks to be mined, then run the `verify` function
+    await simpleStorage.deployTransaction.wait(6) 
+    
+    // since our verify function is `async` we're going to add the `await` key word
+    await verify(simpleStorage.address, [])
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current value is: ${currentValue}`)
+
+  // update the current value
+  const transactionResponse = await simpleStorage.store(69)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated value is: ${updatedValue}`)
+
 }
 
 // args here would be `arguments` for our constructor, if and when we have them
